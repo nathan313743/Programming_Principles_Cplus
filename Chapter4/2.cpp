fba3193552c3441a2520818e25b9d1dd3fd45806
Nathan <nathanflynn01@gmail.com>
2014-01-04 14:31:50 +0000

    Message

---
+++ Chapter4/2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+  vector<double> temperatures;
+  double temp;
+  
+  while(cin >> temp)
+  {
+    temperatures.push_back(temp);
+  }
+  
+  sort(temperatures.begin(), temperatures.end());
+  
+  
+  if(temperatures.size() % 2 == 1)
+  {
+    // it's odd number
+    cout << "Median: " << temperatures[temperatures.size() / 2] << endl;
+  }
+  else
+  {
+    //it's even
+    int size = temperatures.size();
+    double num1 = temperatures[size/2];
+    double num2 = temperatures[(size/2) -1];
+    
+    cout << "Median: " << ((num1+num2) / 2) << endl;
+    
+  }
+  
+  return 0;
+}
