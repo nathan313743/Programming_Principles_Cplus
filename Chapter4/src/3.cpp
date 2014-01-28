506bc577ddee918906cc7e87cc088381ba346af9
nathan313743 <nathandevelopmail@gmail.com>
2014-01-28 19:14:55 +0000

    :P

--- Chapter4/src/3.cpp
+++ Chapter4/src/3.cpp
@@ -1,35 +1,35 @@
-
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-int main()
-{
-  vector<double> distanceVec;
-  double temp;
-
-  while(cin >> temp)
-  {
-    distanceVec.push_back(temp);
-  }
-
-  sort(distanceVec.begin(), distanceVec.end());
-  
-  double totalDistance;
-  
-  for(int i = 0; i < distanceVec.size(); ++i)
-  {
-	  totalDistance += distanceVec[i];
-  }
-  cout << "Total Distance: " << totalDistance << endl;
-  
-  
-  cout << "Smallest distance: " << distanceVec.begin();
-  cout << "Greatest distance: " << distanceVec.end();
-  cout << "Mean distance: " << totalDistance/distanceVec.size();
-
-  return 0;
-}
+//
+//
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int main()
+//{
+//  vector<double> distanceVec;
+//  double temp;
+//
+//  while(cin >> temp)
+//  {
+//    distanceVec.push_back(temp);
+//  }
+//
+//  sort(distanceVec.begin(), distanceVec.end());
+//
+//  double totalDistance;
+//
+//  for(int i = 0; i < distanceVec.size(); ++i)
+//  {
+//	  totalDistance += distanceVec[i];
+//  }
+//  cout << "Total Distance: " << totalDistance << endl;
+//
+//
+//  cout << "Smallest distance: " << distanceVec.begin();
+//  cout << "Greatest distance: " << distanceVec.end();
+//  cout << "Mean distance: " << totalDistance/distanceVec.size();
+//
+//  return 0;
+//}
--- Chapter4/src/4.cpp
+++ Chapter4/src/4.cpp
@@ -1,69 +1,69 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-int main()
-{
-  int foundNumber = 0;
-  int userNum = 0;
-  int numOfGuesses = 0;
-  int endPoint = 100;
-  int startPoint = 1;
-  int midPoint = 50;
-  int temp;
-  bool numberGuessed;
-  string in;
-  
-  cout << "Enter a number for me to guess" << endl;
-  cin >> userNum;
-  
-  while(!numberGuessed)
-  {
-	  if(userNum == midPoint)
-	  {
-		  numberGuessed = true;
-		  continue;
-	  }
-	  cout << "Is the number greater than " << midPoint << "?" << endl;
-	  cin >> in;
-	  
-	  if(in =="y")
-	  {
-		startPoint = midPoint;
-		temp = (endPoint - midPoint) / 2;
-		midPoint = midPoint + temp;
-	  }
-	  else
-	  {
-		endPoint = midPoint;
-		temp = (midPoint - startPoint) / 2;
-		midPoint = midPoint - temp;
-      }  
-      ++numOfGuesses;
-	  
-  }
-  
-  cout << "Your number is :" << midPoint << endl;
-  cout << "Number of guesses :" << midPoint << endl;
-  
-  
-  
-  //if(userNum >= midPoint /*50*/)
-  //{
-	  
-	  //temp = (endPoint - midPoint) / 2;
-	  //midPoint = midPoint + temp;
-	  
-	  //if(userNum >= midPoint /*75*/)
-	  //{
-		//temp = (endPoint - midPoint) / 2;
-		//midPoint = midPoint + temp;
-	  //}
-	  
-  //}
-
-
-  return 0;
-}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int main()
+//{
+//  int foundNumber = 0;
+//  int userNum = 0;
+//  int numOfGuesses = 0;
+//  int endPoint = 100;
+//  int startPoint = 1;
+//  int midPoint = 50;
+//  int temp;
+//  bool numberGuessed;
+//  string in;
+//
+//  cout << "Enter a number for me to guess" << endl;
+//  cin >> userNum;
+//
+//  while(!numberGuessed)
+//  {
+//	  if(userNum == midPoint)
+//	  {
+//		  numberGuessed = true;
+//		  continue;
+//	  }
+//	  cout << "Is the number greater than " << midPoint << "?" << endl;
+//	  cin >> in;
+//
+//	  if(in =="y")
+//	  {
+//		startPoint = midPoint;
+//		temp = (endPoint - midPoint) / 2;
+//		midPoint = midPoint + temp;
+//	  }
+//	  else
+//	  {
+//		endPoint = midPoint;
+//		temp = (midPoint - startPoint) / 2;
+//		midPoint = midPoint - temp;
+//      }
+//      ++numOfGuesses;
+//
+//  }
+//
+//  cout << "Your number is :" << midPoint << endl;
+//  cout << "Number of guesses :" << midPoint << endl;
+//
+//
+//
+//  //if(userNum >= midPoint /*50*/)
+//  //{
+//
+//	  //temp = (endPoint - midPoint) / 2;
+//	  //midPoint = midPoint + temp;
+//
+//	  //if(userNum >= midPoint /*75*/)
+//	  //{
+//		//temp = (endPoint - midPoint) / 2;
+//		//midPoint = midPoint + temp;
+//	  //}
+//
+//  //}
+//
+//
+//  return 0;
+//}
